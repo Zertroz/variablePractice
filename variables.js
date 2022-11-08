2eccc1d49ef3249a56d0d73813048468e21861b6
Kass Daniello <zertroz@gmail.com>
2022-11-08 16:35:32 -0500

    Add reassignments and logs

--- variables.js
+++ variables.js
@@ -4,3 +4,9 @@ var age = 24
 var numberOfPets = 2
 var awake = true
 var asleep = false
+greeting = "Welcome!"
+leaving = "See you later!"
+console.log (greeting)
+console.log (leaving)
+console.log (awake)
+console.log (numberOfPets)
